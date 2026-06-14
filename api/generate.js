110f5142a7bbf1ca3ce429f75ddd8f133dac44e2
Durga Prasad Mishra <97727692+Durgamishra@users.noreply.github.com>
2026-06-14 22:27:01 +0530

    Update and rename server.js to api/generate.js

---
+++ api/generate.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const response = await axios.post(
+            "https://backend-askly-enf76s0ed-durgamishras-projects.vercel.app/generate",
+            { prompt }
+        );
+
+        res.status(200).json(response.data);
+
+    } catch (error) {
+        console.error(error.message);
+
+        res.status(500).json({
+            response: "Backend connection failed"
+        });
+    }
+};
+module.export=app;
